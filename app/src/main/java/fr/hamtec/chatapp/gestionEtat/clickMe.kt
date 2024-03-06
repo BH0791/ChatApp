1b0c7c25325f4d7d8f196fb71aa21e3912d08870
HamidB <ejecteur@yahoo.fr>
2024-03-06 14:19:10 +0100

    init liste de personnes

--- app/src/main/java/fr/hamtec/chatapp/gestionEtat/clickMe.kt
+++ app/src/main/java/fr/hamtec/chatapp/gestionEtat/clickMe.kt
@@ -2,18 +2,21 @@ package fr.hamtec.chatapp.gestionEtat
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import fr.hamtec.chatapp.composables.Screen
 
 @Composable
 fun MyClick(){
-    var clickNumber by remember {
+    var clickNumber by rememberSaveable {
         mutableStateOf(0)
     }
     Column {
@@ -24,7 +27,11 @@ fun MyClick(){
 
 @Composable
 fun IncrementButton(clickNumber: Int, onIncrement: () -> Unit) {
-    Button(onClick = onIncrement) {
+    Button(
+        onClick = onIncrement,
+        colors = if (clickNumber > 5) ButtonDefaults.buttonColors(Color.Yellow)else
+        ButtonDefaults.buttonColors()
+    ) {
         Text(text = "Cliquer")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "ChatApp"
 include(":app")
- 
+include(":listedepersonne")
