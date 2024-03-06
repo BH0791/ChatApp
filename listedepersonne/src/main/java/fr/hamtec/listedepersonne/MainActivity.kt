081921ff896401d6881498493790c10a7482ca5f
HamidB <ejecteur@yahoo.fr>
2024-03-06 15:44:51 +0100

    liste de personnes

--- listedepersonne/src/main/java/fr/hamtec/listedepersonne/MainActivity.kt
+++ listedepersonne/src/main/java/fr/hamtec/listedepersonne/MainActivity.kt
@@ -4,16 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import fr.hamtec.listedepersonne.ui.theme.ChatAppTheme
 
@@ -32,14 +25,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyScreen() {
-    Column( modifier = Modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())
-    ){
-        Data.listePersons.forEach{
-            PersonneItem(fullName = it)
-        }
+// ! ne cosomme pas beaucoup
+LazyColumn {
+    items(Data.listePersons){fullName ->
+        PersonneItem(fullName = fullName)
     }
+}
+// ! Consomme beaucoup si la liste est grande
+//    Column( modifier = Modifier
+//        .fillMaxSize()
+//        .verticalScroll(rememberScrollState())
+//    ){
+//        Data.listePersons.forEach{
+//            PersonneItem(fullName = it)
+//        }
+//    }
 }
 
 @Preview(showBackground = true)
