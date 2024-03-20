4ec3e8863ef304e8656e97e8070842a357ad0aed
HamidB <ejecteur@yahoo.fr>
2024-03-20 17:42:53 +0100

    Update

--- tpbookjetpackcompose/src/main/java/fr/hamtec/tpbookjetpackcompose/chapitre_3/basesDeJetCom.kt
+++ tpbookjetpackcompose/src/main/java/fr/hamtec/tpbookjetpackcompose/chapitre_3/basesDeJetCom.kt
@@ -1,7 +1,11 @@
 package fr.hamtec.tpbookjetpackcompose.chapitre_3
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -36,15 +40,103 @@ fun UserBadge(username : String) {
         )
     }
 }
-@Composable
-fun DemoTypeSafeModifier(){
-    Row {
 
+@Composable
+fun DemoTypeSafeModifier() {
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = "Tomato",
+            modifier = Modifier
+                .weight(1f)
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFF527526)
+                )
+        )
+        Text(
+            text = "Potato",
+            modifier = Modifier
+                .weight(1f)
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFFFF7526)
+                )
+        )
+    }
+}
+
+@Composable
+fun DemoPlacementAvecBox() {
+    Row(Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .size(80.dp, 40.dp)
+                .alignBy { it.measuredHeight / 1 }
+                .background(Color.Magenta)
+        )
+        Box(
+            modifier = Modifier
+                .size(80.dp, 20.dp)
+                .alignBy { it.measuredHeight / 2 }
+                .background(Color.Green)
+        )
+        Box(
+            modifier = Modifier
+                .size(20.dp, 20.dp)
+                .background(Color.Blue)
+        )
+    }
+}
+
+@Composable
+fun DemoPlacementBoxBaseLigne() {
+    Row((Modifier.fillMaxWidth())) {
+        Box(
+            modifier = Modifier
+                .size(80.dp, 40.dp)
+                .alignBy { it.measuredHeight / 3 }
+                .background(Color.Magenta)
+        )
+        Box(
+            modifier = Modifier
+                .size(80.dp, 20.dp)
+                .alignBy { it.measuredHeight / 2 }
+                .background(Color.Green)
+        )
+    }
+}
+
+@Composable
+fun DemoPlacementBoxBase() {
+    Row(Modifier.fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .size(80.dp, 40.dp)
+                .alignBy { it.measuredHeight / 1 }
+                .background(Color.Magenta)
+        )
+        Box(
+            modifier = Modifier
+                .size(80.dp, 20.dp)
+                .alignBy { it.measuredHeight / 2 }
+                .background(Color.Green)
+        )
+        Box(
+            modifier = Modifier
+                .size(20.dp, 20.dp)
+                .background(Color.Blue)
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun UserBadgePreview() {
-    UserBadge(username = "Louis Hamstrong")
+    //UserBadge(username = "Louis Hamstrong")
+    //DemoTypeSafeModifier()
+    //DemoPlacementAvecBox()
+    //DemoPlacementBoxBaseLigne()
+    DemoPlacementBoxBase()
 }
