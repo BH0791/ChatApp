00e7528a2fb016d2f0a894631559d9e567f94294
HamidB <ejecteur@yahoo.fr>
2024-03-26 10:59:02 +0100

    Update DemoViewModel

--- demoviewmodel/src/main/java/fr/hamtec/demoviewmodel/ui/GameScreen.kt
+++ demoviewmodel/src/main/java/fr/hamtec/demoviewmodel/ui/GameScreen.kt
@@ -1,3 +1,67 @@
 package fr.hamtec.demoviewmodel.ui
+
+
+import android.app.Activity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.shapes
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import fr.hamtec.demoviewmodel.R
+
 //- Tous les composables de l'interface utilisateur sont définis dans le fichier GameScreen.kt.
 //- Les sections suivantes vous présentent quelques fonctions modulables.
+
+
+/**
+ * GameStatus est une fonction composable qui affiche le score du jeu en bas de l'écran. La fonction
+ * composable contient un composable Text dans une Card. Pour l'instant, le score est codé en dur pour être
+  */
+
+@Composable
+fun GameStatus(score: Int, modifier: Modifier = Modifier) {
+    Card(
+            modifier = modifier
+    ) {
+        Text(
+                text = stringResource(R.string.score, score),
+                style = typography.headlineMedium,
+                modifier = Modifier.padding(8.dp)
+        )
+
+    }
+}
