08e9b4348e0a610a708a92672814903f1986f33e
HamidB <ejecteur@yahoo.fr>
2024-03-26 09:35:11 +0100

    Init module

--- tpviewmodel/src/main/java/fr/hamtec/tpviewmodel/vui/UiEvent.kt
+++ tpviewmodel/src/main/java/fr/hamtec/tpviewmodel/vui/UiEvent.kt
@@ -2,6 +2,12 @@ package fr.hamtec.tpviewmodel.vui
 
 // ** Tous les éléments de l'écran action
 sealed class UiEvent {
+    // ** Dans Kotlin, les objets sont utilisés pour créer des instances singleton ou pour regrouper
+    // ** des fonctionnalités associées. Nous pouvons accéder et utiliser sans effort les objets de
+    //  **l’instance singleton sans avoir besoin de modèles singleton explicites ou de déclarations statiques.
+    // ** Il est pratique d’utiliser des objets pour maintenir un état global, fournir des fonctionnalités
+    // ** centralisées ou gérer des opérations à l’échelle de l’application.
+
     object OnInit: UiEvent()    // **class object, cette classe va hériter de la classe UiEvent sans paramètre
     object OnGenerateClick: UiEvent()
 }
